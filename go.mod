94caee66d0e03e9f28a8cde27c653b769fadf970
Piyush <piyushsharma2418@gmail.com>
2023-11-01 13:08:23 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/piyush/golang-chat
+
+go 1.21.3
+
+require github.com/gorilla/websocket v1.5.0
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/piyush/golang-chat/pkg/websocket"
+)
+
+func serveWS(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("websocket endpoint reached")
+
+	conn, err := websocket.Upgrade(w, r)
+
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Golang chat backend")
+	setupRoutes()
+	http.ListenAndServe(":8000", nil)
+
+}
